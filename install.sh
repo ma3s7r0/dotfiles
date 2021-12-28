6f40dae380f6957be5b267d7d9e1f45ff4f5a9d5
ma3s7r0 <ma3s7r0@gmail.com>
2021-12-28 14:00:23 +0000

    Initial commit

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+cd $HOME
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+ln -s dotfiles/.zshrc
+ln -s dotfiles/.p10k.zsh
