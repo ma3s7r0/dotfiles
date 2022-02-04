4230448a70c81d7f5ef3e9e69bcd4e7547b89efe
Klinck Nico <Nico.Klinck@progroup.ag>
2022-02-04 17:57:42 +0100

    Added Alias

---
+++ virtualbox.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+/usr/bin/VBoxClient --clipboard && /usr/bin/VBoxClient --checkhostversion && /usr/bin/VBoxClient --seamless && /usr/bin/VBoxClient --draganddrop && /usr/bin/VBoxClient --vmsvga
